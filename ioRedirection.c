4250387a16788ade285f0b02450a989dfb5d8405
sambuaneesh <sambuaneesh@gmail.com>
2023-09-11 19:14:56 +0530

    initial io redirec

--- ioRedirection.c
+++ ioRedirection.c
@@ -1,7 +1,7 @@
 #include "headers.h"
 #include "tools.h"
 
-void ioRedirection(char *command) {
+void writeFile(char *command) {
     char *commandCopy = malloc(strlen(command) + 1);
     strcpy(commandCopy, command);
 
@@ -34,4 +34,94 @@ void ioRedirection(char *command) {
     free(commandCopy);
     free(commandToExecute);
     free(fileName);
+}
+
+void readFile(char *command) {
+    char *commandCopy = malloc(strlen(command) + 1);
+    strcpy(commandCopy, command);
+
+    char *token = strtok(commandCopy, "<");
+    char *commandToExecute = malloc(strlen(token) + 1);
+    strcpy(commandToExecute, token);
+
+    token = strtok(NULL, "<");
+    char *fileName = malloc(strlen(token) + 1);
+    strcpy(fileName, token);
+
+    trim(commandToExecute);
+    trim(fileName);
+
+    int fd = open(fileName, O_RDONLY);
+    if (fd < 0) {
+        perror("Error opening file");
+        return;
+    }
+
+    int stdinCopy = dup(STDIN_FILENO);
+    dup2(fd, STDIN_FILENO);
+    close(fd);
+
+    executeCommand(commandToExecute, 0);
+
+    dup2(stdinCopy, STDIN_FILENO);
+    close(stdinCopy);
+
+    free(commandCopy);
+    free(commandToExecute);
+    free(fileName);
+}
+
+void ioRedirection(char *command) {
+//    check if there is a pipe
+    if (strchr(command, '|') != NULL) {
+        pipeCommand(command);
+        return;
+    }
+
+    if (strchr(command, '<') != NULL) {
+        readFile(command);
+        return;
+    }
+
+//    if there is a >>, append to file
+    if (strstr(command, ">>") != NULL) {
+        char *commandCopy = malloc(strlen(command) + 1);
+        strcpy(commandCopy, command);
+
+        char *token = strtok(commandCopy, ">>");
+        char *commandToExecute = malloc(strlen(token) + 1);
+        strcpy(commandToExecute, token);
+
+        token = strtok(NULL, ">>");
+        char *fileName = malloc(strlen(token) + 1);
+        strcpy(fileName, token);
+
+        trim(commandToExecute);
+        trim(fileName);
+
+        int fd = open(fileName, O_WRONLY | O_CREAT | O_APPEND, 0644);
+        if (fd < 0) {
+            perror("Error opening file");
+            return;
+        }
+
+        int stdoutCopy = dup(STDOUT_FILENO);
+        dup2(fd, STDOUT_FILENO);
+        close(fd);
+
+        executeCommand(commandToExecute, 0);
+
+        dup2(stdoutCopy, STDOUT_FILENO);
+        close(stdoutCopy);
+
+        free(commandCopy);
+        free(commandToExecute);
+        free(fileName);
+        return;
+    }
+
+    if (strchr(command, '>') != NULL) {
+        writeFile(command);
+        return;
+    }
 }
